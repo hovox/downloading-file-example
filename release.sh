9c70d555cfe563ea77b5128656cc040c50612ec1
hov <hov@picsart.com>
2016-08-30 20:35:32 +0400

    fdsafs

--- release.sh
+++ release.sh
@@ -15,12 +15,14 @@ git tag $versionstring;
 git push --tags;
 params="{\"tag_name\": \"$versionstring\", \"target_commitish\": \"live\", \"name\": \"$versionstring\", \"body\": \"$1\" }"
 echo "params are $params"
-curl -i -X POST https://api.github.com/repos/hovox/downloading-file-example/releases?access_token=c625076d2c7826ed0d97427322a1ad92c00594e3 -d "$params" > /tmp/result.txt;
-status=$(cat /tmp/result.txt | grep "Status: 201 Created");
-cat /tmp/result.txt
-echo "status is $status"
-if [ "$status" != "Status: 201 Created" ]; then
-        (>&2 echo "failed to create release on github $status"); exit -1;
+curl -i -X POST https://api.github.com/repos/hovox/downloading-file-example/releases?access_token=e1e1e1e80f3c414f8c45256217db7c16b0b4901c -d "$params" > /tmp/result.txt;
+stt=$(cat /tmp/result.txt | grep "Status: 201 Created" | tr -d '\r' | tr -d '\n');
+
+echo "$stt"
+
+
+if [ "$stt" != "Status: 201 Created" ]; then
+        (>&2 echo "failed to create release on github $stt"); exit -1;
 fi;
 git checkout release;
 git pull;
